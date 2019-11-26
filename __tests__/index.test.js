67fe03f003657e406e34c990d96c23d2238734eb
Konstantin Lukyanenok <loukianen@gmail.com>
2019-11-26 09:28:36 +0300

    step 6

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,12 @@
 import fs from 'fs';
 import genDiff from '../src/';
-// import {getFullPath, getDifference} from '../src/';
 
 const fileJson1 = `${__dirname}/__fixtures__/before.json`;
 const fileJson2 = `${__dirname}/__fixtures__/after.json`;
 const fileYml1 = `${__dirname}/__fixtures__/before.yml`;
 const fileYml2 = `${__dirname}/__fixtures__/after.yml`;
+const fileIni1 = `${__dirname}/__fixtures__/before.ini`;
+const fileIni2 = `${__dirname}/__fixtures__/after.ini`;
 const etalon = `${__dirname}/__fixtures__/etalon`;
 const getConsts = (firstFile, secondFile) => {
   const expectResult = fs.readFileSync(etalon, 'utf8').split('\n');
@@ -13,28 +14,13 @@ const getConsts = (firstFile, secondFile) => {
   const wrongResult = genDiff(firstFile, secondFile).split('\n').slice(1);
   return [expectResult, result, wrongResult];
 };
-const constsJson = getConsts(fileJson1, fileJson2);
-const constsYml = getConsts(fileYml1, fileYml2);
-test('getDiffJsonTrue', () => {
-  expect(constsJson[1]).toEqual(expect.arrayContaining(constsJson[0]));
-  expect(constsJson[0]).toEqual(expect.arrayContaining(constsJson[1]));
-})
-test('ggetDiffJsonFalse', () => {
-  expect(constsJson[2]).not.toEqual(expect.arrayContaining(constsJson[0]));
-})
-test('getDiffYmlTrue', () => {
-  expect(constsYml[1]).toEqual(expect.arrayContaining(constsYml[0]));
-  expect(constsYml[0]).toEqual(expect.arrayContaining(constsYml[1]));
-})
-test('ggetDiffYmlFalse', () => {
-  expect(constsYml[2]).not.toEqual(expect.arrayContaining(constsYml[0]));
-})
-
-/* test('getFullPath', () => {
-  expect(getFullPath('/home/lkf/projects/frontend-project-lvl2/data/before.json')).toBe('/home/lkf/projects/frontend-project-lvl2/data/before.json');
-  expect(getFullPath('data/before.json')).toBe('/home/lkf/projects/frontend-project-lvl2/data/before.json');
-  expect(getFullPath('./data/before.json')).toBe('/home/lkf/projects/frontend-project-lvl2/data/before.json');
-  expect(getFullPath('../../data/before.json')).toBe('/home/lkf/data/before.json');
-  expect(getFullPath('before.json')).toBe('/home/lkf/projects/frontend-project-lvl2/before.json');
-})*/
+test.each([
+  getConsts(fileJson1, fileJson2),
+  getConsts(fileYml1, fileYml2),
+  getConsts(fileIni1, fileIni2),
+])('getDiff%# json, yml, ini', (expRes, res, wroRes) => {
+  expect(res).toEqual(expect.arrayContaining(expRes));
+  expect(expRes).toEqual(expect.arrayContaining(res));
+  expect(wroRes).not.toEqual(expect.arrayContaining(expRes));
+});
 // that cod id from code-climat 8952ce102d144a6c27b94c0ce7d2a4696597733df4542e17c624b126c83bc3a1
--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,13 @@
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 import fs from 'fs';
 
 export default (filePath) => {
   const parsers = {
     json: (file) => JSON.parse(fs.readFileSync(file, 'utf8')),
     yml: (file) => yaml.safeLoad(fs.readFileSync(file, 'utf8')),
+    ini: (file) => ini.parse(fs.readFileSync(file, 'utf-8')),
   };
   const type = path.extname(filePath).slice(1);
   return parsers[type](filePath);
